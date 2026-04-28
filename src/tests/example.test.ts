8b8da36a53906df358f47fb7d738a934b4517c0c
SnaveSutit <SnaveSutit@gmail.com>
2026-04-28 02:27:20 -0400

    🧪 Create `example.test.ts`

---
+++ src/tests/example.test.ts
@@ -0,0 +1,2 @@
+// Tests cannot test Blockbench-specific functionality, but they can be used to test any pure functions you have in your plugin.
+// You can create as many test files as you want in the `src/tests` directory, and they will be picked up by Vitest automatically.
